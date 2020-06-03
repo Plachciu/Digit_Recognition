9fdc6b5d6debca0d9b77a0cd7e6910de624efed5
Plachciu <placheta.kacper@gmail.com>
2020-06-04 01:10:02 +0200

    Etap pierwszy obiektowo

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -5,59 +5,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Input grid:");
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Character> grid = new ArrayList<>();
-        for (int i = 0; i <  3; i++) {
-            char[] tempChar = scanner.nextLine().toCharArray();
-            for (int j = 0; j < 3; j++) {
-                grid.add(tempChar[j]);
-            }
-        }
+        Recognizer digit = new Recognizer();
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print(grid.get(i));
-        }
-        System.out.println();
-        for (int i = 3; i < 6; i++) {
-            System.out.print(grid.get(i));
-        }
-        System.out.println();
-        for (int i = 6; i < 9; i++) {
-            System.out.print(grid.get(i));
-        }
-        System.out.println();
+        digit.gridReader();
 
-        int [] ifX = new int[9];
-        for (int i = 0; i < 9; i++) {
-            if (grid.get(i) == 'X'){
-                ifX[i] = 1;
-            } else {
-                ifX[i] = 0;
-            }
-        }
+        digit.gridWriter();
 
-        int value = ifX[0] * 2 +
-                ifX[1] * 1 +
-                ifX[2] * 2 +
-                ifX[3] * 4 +
-                ifX[4] * (-4) +
-                ifX[5] * 4 +
-                ifX[6] * 2 +
-                ifX[7] * (-1) +
-                ifX[8] * 2 +
-                (-5);
-
-        int number = 0;
-        if (value > 0) {
-            number = 0;
-        } else if (value < 0){
-            number = 1;
-        }
-
-        System.out.println("This number is " + number);
-
-
-
+        digit.digitRecognizer();
 
     }
 }
--- Digit Recognition/task/src/recognition/Recognizer.java
+++ Digit Recognition/task/src/recognition/Recognizer.java
@@ -21,6 +21,51 @@ public class Recognizer {
         }
     }
 
+    public void gridWriter () {
+
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(this.grid.get(i + j));
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public void digitRecognizer () {
+
+        int [] ifX = new int[9];
+        for (int i = 0; i < 9; i++) {
+            if (this.grid.get(i) == 'X'){
+                ifX[i] = 1;
+            } else {
+                ifX[i] = 0;
+            }
+        }
+
+        int value = ifX[0] * 2 +
+                ifX[1] * 1 +
+                ifX[2] * 2 +
+                ifX[3] * 4 +
+                ifX[4] * (-4) +
+                ifX[5] * 4 +
+                ifX[6] * 2 +
+                ifX[7] * (-1) +
+                ifX[8] * 2 +
+                (-5);
+
+        int number = 0;
+        if (value > 0) {
+            number = 0;
+        } else if (value < 0){
+            number = 1;
+        }
+
+        System.out.println("This number is " + number);
+
+    }
+
 
 
 }
