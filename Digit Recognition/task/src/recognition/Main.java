18c5a40d5e0b752b751d2d80a924a5d74cf5200b
Plachciu <placheta.kacper@gmail.com>
2020-06-08 01:27:06 +0200

    pierwszy checkpoint

--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -4,6 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+//        Map<Integer,Integer> bias = new HashMap<>();
+//        bias.put(0,-1);
+//        bias.put(1,6);
+//        bias.put(2,1);
+//        bias.put(3,0);
+//        bias.put(4,2);
+//        bias.put(5,0);
+//        bias.put(6,-1);
+//        bias.put(7,3);
+//        bias.put(8,-2);
+//        bias.put(9,0);
+
         System.out.println("Input grid:");
         Recognizer digit = new Recognizer();
 
@@ -12,6 +25,10 @@ public class Main {
         digit.gridWriter();
 
         digit.digitRecognizer();
+
+
+
+
 
     }
 }
--- Digit Recognition/task/src/recognition/Recognizer.java
+++ Digit Recognition/task/src/recognition/Recognizer.java
@@ -1,6 +1,7 @@
 package recognition;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Recognizer {
@@ -13,7 +14,7 @@ public class Recognizer {
 
     public void gridReader () {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i <  3; i++) {
+        for (int i = 0; i <  5; i++) {
             char[] tempChar = scanner.nextLine().toCharArray();
             for (int j = 0; j < 3; j++) {
                 this.grid.add(tempChar[j]);
@@ -23,7 +24,7 @@ public class Recognizer {
 
     public void gridWriter () {
 
-        for (int i = 0; i < 9; i += 3) {
+        for (int i = 0; i < 15; i += 3) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(this.grid.get(i + j));
             }
@@ -35,34 +36,87 @@ public class Recognizer {
 
     public void digitRecognizer () {
 
-        int [] ifX = new int[9];
-        for (int i = 0; i < 9; i++) {
-            if (this.grid.get(i) == 'X'){
-                ifX[i] = 1;
+        int [] valueGrid = new int[15];
+        int c = 0;
+        for (char temp : grid) {
+            if (temp == 'X'){
+                valueGrid[c] = 1;
+                c++;
             } else {
-                ifX[i] = 0;
+                valueGrid[c] = -1;
+                c++;
             }
         }
 
-        int value = ifX[0] * 2 +
-                ifX[1] * 1 +
-                ifX[2] * 2 +
-                ifX[3] * 4 +
-                ifX[4] * (-4) +
-                ifX[5] * 4 +
-                ifX[6] * 2 +
-                ifX[7] * (-1) +
-                ifX[8] * 2 +
-                (-5);
-
-        int number = 0;
-        if (value > 0) {
-            number = 0;
-        } else if (value < 0){
-            number = 1;
+        int [] ifX = new int[15];
+        int x = 0;
+        for (char temp : grid) {
+            if (temp == 'X'){
+                ifX[x] = 1;
+                x++;
+            } else {
+                ifX[x] = 0;
+                x++;
+            }
         }
 
-        System.out.println("This number is " + number);
+
+        int value = 0;
+        for (int i = 0; i < grid.size(); i++){
+            value += valueGrid[i] + ifX[i];
+        }
+//        spróbować dokończyć na enumach!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int [] ifX = new int[9];
+//        for (int i = 0; i < 9; i++) {
+//            if (this.grid.get(i) == 'X'){
+//                ifX[i] = 1;
+//            } else {
+//                ifX[i] = 0;
+//            }
+//        }
+//
+//        int value = ifX[0] * 2 +
+//                ifX[1] * 1 +
+//                ifX[2] * 2 +
+//                ifX[3] * 4 +
+//                ifX[4] * (-4) +
+//                ifX[5] * 4 +
+//                ifX[6] * 2 +
+//                ifX[7] * (-1) +
+//                ifX[8] * 2 +
+//                (-5);
+//
+//        int number = 0;
+//        if (value > 0) {
+//            number = 0;
+//        } else if (value < 0){
+//            number = 1;
+//        }
+//
+//        System.out.println("This number is " + number);
 
     }
 
